7b35558052d0c263c4f0ceda0d74b1a61da63ee7
Louis Jean ARNAUD <bones.you.spooky@gmail.com>
2018-02-16 17:46:32 +0100

    GSB_LL - CompteRendu - INDEV0.4
    - MODIFIED : CompteRendu is in the good place

--- controleurs/c_compteRendu.php
+++ controleurs/c_compteRendu.php
@@ -1,3 +1,20 @@
 <?php
-
+include("vues/v_sommaire.php");
+$action = $_REQUEST['action'];
+switch($action)
+{
+	case 'saisirCompteRendu' :
+	{
+		
+	}
+	case 'validerCompteRendu':
+	{
+		
+	}
+	case 'ListeCompteRendu':
+	{
+		//$comptesRendus = $pdo->getComptesRendus($idVisiteur)
+	}
+	
+}
 ?>
--- vues/v_compteRendu.php
+++ vues/v_compteRendu.php
@@ -1,14 +1,2 @@
 <?php
-include("vues/v_sommaire.php");
-$action = $_REQUEST['action'];
-switch($action)
-{
-	case 'afficherCompteRendu':
-	{
-		//$comptesRendus = $pdo->getComptesRendus($idVisiteur)
-	}
-	case 'ajouterCompteRendu'
-	{
-
-	}
-}
+?>
