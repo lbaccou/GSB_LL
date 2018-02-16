4e146ab8a39f9028e54fe76d521f463135d52c2e
Louis Jean ARNAUD <bones.you.spooky@gmail.com>
2018-02-16 15:36:58 +0100

    GSB_LL - CompteRendu - INDEV 0.3
    - MODIFIED : Another one

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -20,7 +20,7 @@
         <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
       </li>
       <li class="smenu">
-        <a href="index.php?uc=compteRendu&action=afficherCompteRendu" title="Saisie d'un compte-rendu">Comptes-rendus</a>
+        <a href="index.php?uc=compteRendu&action=afficherCompteRendu" title="Saisie d'un compte-rendu">Compte-rendu</a>
       </li>
       <li class="smenu">
         <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
